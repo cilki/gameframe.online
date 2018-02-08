c7dcbff09b160b7b60e4e762b05165c84f4f22ca
Sam Faulkner <srf.000@hotmail.com>
2018-02-07 18:19:32 -0600

    Added the default bootstrap carousel to splash page
    The splash page will contain the carousel defined in the project
    requirements for phase 1

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,13 +1,37 @@
 
 
-import { Component } from 'react';
+import React from 'react';
 
-class SplashPage extends Component {
+import { Carousel, Image } from 'react-bootstrap'; 
 
+class SplashPage extends React.Component {
 
 	render() {
+
 		return (
-			<h1>Hello World</h1>
+			<Carousel>
+			  <Carousel.Item>
+			    <img width={900} height={500} alt="900x500" src="/carousel.png" />
+			    <Carousel.Caption>
+			      <h3>First slide label</h3>
+			      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+			    </Carousel.Caption>
+			  </Carousel.Item>
+			  <Carousel.Item>
+			    <img width={900} height={500} alt="900x500" src="/carousel.png" />
+			    <Carousel.Caption>
+			      <h3>Second slide label</h3>
+			      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+			    </Carousel.Caption>
+			  </Carousel.Item>
+			  <Carousel.Item>
+			    <img width={900} height={500} alt="900x500" src="/carousel.png" />
+			    <Carousel.Caption>
+			      <h3>Third slide label</h3>
+			      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+			    </Carousel.Caption>
+			  </Carousel.Item>
+			</Carousel>
 		);
 	}
 }
