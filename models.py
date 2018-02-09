cf76bf009e1cb98aa3427d66fdd26fb8215fc6e3
Ekin Soysal <ekinsoysal@hotmail.com>
2018-02-09 17:21:34 -0600

    Fixed DB Schema spacing

--- models.py
+++ models.py
@@ -11,60 +11,60 @@ db = SQLAlchemy()
 
 game_article = db.Table('game_article',
     db.Column('game_id', db.Integer, db.ForeignKey('game_id'), primary_key = True),
-	db.Column('article_id', db.Integer, db.ForeignKey('article_id'), primary_key = True)
+    db.Column('article_id', db.Integer, db.ForeignKey('article_id'), primary_key = True)
 )
 
 game_developer = db.Table('game_developer',
     db.Column('game_id', db.Integer, db.ForeignKey('game_id'), primary_key = True),
-	db.Column('developer_id', db.Integer, db.ForeignKey('developer_id'), primary_key = True)
+    db.Column('developer_id', db.Integer, db.ForeignKey('developer_id'), primary_key = True)
 )
 
 game_tweet = db.Table('game_tweet',
     db.Column('game_id', db.Integer, db.ForeignKey('game_id'), primary_key = True),
-	db.Column('tweet_id', db.Integer, db.ForeignKey('tweet_id'), primary_key = True)
+    db.Column('tweet_id', db.Integer, db.ForeignKey('tweet_id'), primary_key = True)
 )
 
 game_video = db.Table('game_video',
     db.Column('game_id', db.Integer, db.ForeignKey('game_id'), primary_key = True),
-	db.Column('video_id', db.Integer, db.ForeignKey('video_id'), primary_key = True)
+    db.Column('video_id', db.Integer, db.ForeignKey('video_id'), primary_key = True)
 )
 
 game_stream = db.Table('game_stream',
     db.Column('game_id', db.Integer, db.ForeignKey('game_id'), primary_key = True),
-	db.Column('stream_id', db.Integer, db.ForeignKey('stream_id'), primary_key = True)
+    db.Column('stream_id', db.Integer, db.ForeignKey('stream_id'), primary_key = True)
 )
 
 article_developer = db.Table('article_developer',
     db.Column('article_id', db.Integer, db.ForeignKey('article_id'), primary_key = True),
-	db.Column('developer_id', db.Integer, db.ForeignKey('developer_id'), primary_key = True)
+    db.Column('developer_id', db.Integer, db.ForeignKey('developer_id'), primary_key = True)
 )
 
 developer_tweet = db.Table('developer_tweet',
     db.Column('developer_id', db.Integer, db.ForeignKey('developer_id'), primary_key = True)
-	db.Column('tweet_id', db.Integer, db.ForeignKey('tweet_id'), primary_key = True)
+    db.Column('tweet_id', db.Integer, db.ForeignKey('tweet_id'), primary_key = True)
 )
 
 class Game(db.Model):
     game_id = db.Column(db.Integer, primary_key = True)
-	steam_id = db(db.Integer)
+    steam_id = db(db.Integer)
     name = db.Column(db.String(120), unique = True, nullable = False)
     release = db.Column(db.Integer, nullable = False)
-	image = db.Column(db.Text, nullable = False)
-	genre = db.Column(db.String(80), nullable = False)
+    image = db.Column(db.Text, nullable = False)
+    genre = db.Column(db.String(80), nullable = False)
 	
-	tweets = db.relationship('Tweet', secondary = game_tweet, back_populates = "games")
-	videos = db.relationship('YouTube Video', secondary = game_video, back_populates = "games")
-	streams = db.relationship('Twitch Channel', secondary = game_stream, back_populates = "games")
-	articles = db.relationship('Article',  secondary = game_article, back_populates = "games")
-	developers = db.relationship('Developer', secondary = game_developer, back_populates = "games")
+    tweets = db.relationship('Tweet', secondary = game_tweet, back_populates = "games")
+    videos = db.relationship('YouTube Video', secondary = game_video, back_populates = "games")
+    streams = db.relationship('Twitch Channel', secondary = game_stream, back_populates = "games")
+    articles = db.relationship('Article',  secondary = game_article, back_populates = "games")
+    developers = db.relationship('Developer', secondary = game_developer, back_populates = "games")
 
-	def __repr__(self):
+    def __repr__(self):
         return '<Game %r>' % self.name
 
 class Article(db.Model):
     article_id = db.Column(db.Integer, primary_key = True)
     title = db.Column(db.String(120), nullable = False)
-	outlet = db.Column(db.String(120), nullable = False)
+    outlet = db.Column(db.String(120), nullable = False)
     author = db.Column(db.String(120), nullable = False)
     timestamp = db.Column(db.DateTime, nullable = False)
     image = db.Column(db.Text, nullable = False)
@@ -81,21 +81,21 @@ class Developer(db.Model):
     name = db.Column(db.String(120), unique = True, nullable = False)
     country = db.Column(db.String(120), nullable = False)
     logo = db.Column(db.Text, nullable = False)
-	foundation = db.Column(db.Integer, nullable = False)
+    foundation = db.Column(db.Integer, nullable = False)
     
     games = db.relationship('Game', secondary = game_developer, back_populates = "developers")
     articles = db.relationship('Article', secondary = article_developer, back_populates = "developers")
-	tweets = db.relationship('Tweet', secondary = developer_tweet, back_populates = "developers")
+    tweets = db.relationship('Tweet', secondary = developer_tweet, back_populates = "developers")
 
     def __repr__(self):
         return '<Developer %r>' % self.name
 		
 class Tweet(db.Model):
     tweet_id_id = db.Column(db.Integer, primary_key = True)
-	twitter_id = db.Column(db.Integer, nullable = False)
+    twitter_id = db.Column(db.Integer, nullable = False)
     content = db.Column(db.String(280), nullable = False)
-	user = db.Column(db.String(80), nullable = False)
-	timestamp = db.Column(db.DateTime, nullable = False)
+    user = db.Column(db.String(80), nullable = False)
+    timestamp = db.Column(db.DateTime, nullable = False)
 
     games = db.relationship('Game', secondary = game_tweet, back_populates = "tweets")
     developers = db.relationship('Developer', secondary = article_tweet, back_populates = "tweets")
@@ -105,10 +105,10 @@ class Tweet(db.Model):
 
 class Video(db.Model):
     video_id = db.Column(db.Integer, primary_key = True)
-	youtube_id = db.Column(db.Integer, nullable = False)
+    youtube_id = db.Column(db.Integer, nullable = False)
     name = db.Column(db.String(120), nullable = False)
     channel = db.Column(db.String(120), nullable = False)
-	timestamp = db.Column(db.DateTime, nullable = False)
+    timestamp = db.Column(db.DateTime, nullable = False)
 
     games = db.relationship('Game', secondary = game_video, back_populates = "videos")
 
