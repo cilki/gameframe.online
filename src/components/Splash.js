930ae5f43a50294a525636cc7ab4ea979f14a8d4
Mitchell Roger Marshe <mitchellmarshe@gmail.com>
2018-02-10 17:39:13 -0600

    Centered and Linked Images

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -4,7 +4,6 @@
 
 import React from 'react';
 import { Carousel, Image } from 'react-bootstrap'; 
-// import CarouselItem from './CarouselItem';
 
 const games = [
 	'/static/data/games/PLAYERUNKNOWN\'S BATTLEGROUNDS.json',
@@ -38,19 +37,26 @@ class SplashPage extends React.Component {
 				}}
 			>
 				<Carousel>
-				  {/*childComponents*/}
-				  <Carousel.Item>
-				    <img width={900} height={500} alt="900x500" src="http://cdn.akamai.steamstatic.com/steam/apps/578080/header.jpg?t=1516879634" />
-				  </Carousel.Item>
-				  <Carousel.Item>
-				    <img width={900} height={500} alt="900x500" src="http://cdn.akamai.steamstatic.com/steam/apps/620/header.jpg?t=1512411524" />
-				  </Carousel.Item>
-				  <Carousel.Item>
-				    <img width={900} height={500} alt="900x500" src="http://cdn.akamai.steamstatic.com/steam/apps/252950/header.jpg?t=1512003663" />
-				  </Carousel.Item>
-				  <Carousel.Item>
-				    <img width={900} height={500} alt="900x500" src="http://cdn.akamai.steamstatic.com/steam/apps/242760/header.jpg?t=1478631950" />
-				  </Carousel.Item>
+					<Carousel.Item>
+						<a href="/games" target="_blank">
+							<img style={{margin: 'auto'}} width={900} height={500} alt="PLAYERUNKNOWN'S BATTLEGROUNDS" src="http://cdn.akamai.steamstatic.com/steam/apps/578080/header.jpg?t=1516879634" />
+						</a>
+					</Carousel.Item>
+					<Carousel.Item>
+						<a href="/games" target="_blank">
+							<img style={{margin: 'auto'}} width={900} height={500} alt="Portal 2" src="http://cdn.akamai.steamstatic.com/steam/apps/620/header.jpg?t=1512411524" />
+						</a>
+					</Carousel.Item>
+					<Carousel.Item>
+						<a href="/games" target="_blank">
+							<img style={{margin: 'auto'}} width={900} height={500} alt="Rocket League" src="http://cdn.akamai.steamstatic.com/steam/apps/252950/header.jpg?t=1512003663" />
+						</a>
+					</Carousel.Item>
+					<Carousel.Item>
+						<a href="/games" target="_blank">
+							<img style={{margin: 'auto'}} width={900} height={500} alt="The Forest" src="http://cdn.akamai.steamstatic.com/steam/apps/242760/header.jpg?t=1478631950" />
+						</a>
+					</Carousel.Item>
 				</Carousel>
 			</div>
 		);
