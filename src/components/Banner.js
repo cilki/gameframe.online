bf971c186127a83ab300392bf689e6f92825b8ac
Mitchell Roger Marshe <mitchellmarshe@gmail.com>
2018-02-10 10:56:28 -0600

    Added Banner to Site

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,13 +1,15 @@
-
 /**
  * Banner component that goes across the top of the screen.
  */
 
 import React from 'react';
+import { Image } from 'react-bootstrap';
 
 class Banner extends React.Component {
 	render() {
-		return null;
+		return (
+			<img height={350} src="https://hdwallsource.com/img/2014/12/video-game-wallpapers-8223-8556-hd-wallpapers.jpg" alt="Video Game Characters" />
+		);
 	}
 }
 
