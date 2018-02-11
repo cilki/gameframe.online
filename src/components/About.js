1fe129409f65a4fd132b25aadeb11ecdf77039de
longhorn396 <devin@drawhorn.com>
2018-02-11 11:01:09 -0600

    Most pictures gotten and number of commits pulled from GitHub

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,31 @@
 import React from 'react';
 
-import {PageHeader, Grid, Row, Col, Image} from 'react-bootstrap'
+import {PageHeader, Grid, Row, Col, Thumbnail} from 'react-bootstrap'
 
 class AboutPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            'cilki' : [0,0,0],
+            'longhorn396': [0,0,0],
+            'therealsamf': [0,0,0],
+            'mitchellmarshe': [0,0,0],
+            'atrieder': [0,0,0],
+            'esoysal': [0,0,0],
+            total: [0,0,0]
+        };
+        fetch('https://api.github.com/repos/cilki/gameframe.online/contributors', {method: 'GET'})
+            .then(response => response.json())
+            .then(json => {
+                for(var i = 0; i < json.length; i++) {
+                    this.state[json[i]['login']][0] = json[i]['contributions'];
+                    this.state.total[0] += json[i]['contributions'];
+                }
+                this.setState(this.state);
+            });
+    }
+
     render() {
 
 		return (
@@ -11,43 +34,109 @@ class AboutPage extends React.Component {
             }}>
                 {/*Team Name*/}
                 <PageHeader>GameFrame LLC </PageHeader>
-                <br/>
+                <hr/>
                 {/*Description and/or Explanation*/}
                 <p>
                     This project was born out of all of our love of video games. TODO add stuff here. 
                 </p>
-                <br/>
+                <hr/>
                 {/*Group Members*/}
                 <Grid>
                     <Row>
-                        <Col xs={12} md={4}>
-                            <Image></Image>
+                        <Col md={4}>
+                            <Thumbnail src="https://avatars3.githubusercontent.com/u/10459406?s=460&v=4">
+                                <h3>Tyler Cook</h3>
+                                <div style={{
+                                    textAlign: 'left'
+                                }}>
+                                    <p><strong>Bio:</strong> </p>
+                                    <p><strong>Responsibilities:</strong> </p>
+                                    <p><strong>Commits: </strong>{this.state['cilki'][0]}</p>
+                                    <p><strong>Issues: </strong>{this.state['cilki'][1]}</p>
+                                    <p><strong>Unit Tests: </strong>{this.state['cilki'][2]}</p>
+                                </div>
+                            </Thumbnail>
                         </Col>
-                        <Col xs={12} md={4}>
-                            <Image></Image>
+                        <Col md={4}>
+                            <Thumbnail src="https://avatars0.githubusercontent.com/u/16629308?s=460&v=4">
+                                <h3>Devin Drawhorn</h3>
+                                <div style={{
+                                    textAlign: 'left'
+                                }}>
+                                    <p><strong>Bio:</strong> </p>
+                                    <p><strong>Responsibilities:</strong> </p>
+                                    <p><strong>Commits: </strong>{this.state['longhorn396'][0]}</p>
+                                    <p><strong>Issues: </strong>{this.state['longhorn396'][1]}</p>
+                                    <p><strong>Unit Tests: </strong>{this.state['longhorn396'][2]}</p>
+                                </div>
+                            </Thumbnail>
                         </Col>
-                        <Col xs={12} md={4}>
-                            <Image></Image>
+                        <Col md={4}>
+                            <Thumbnail src="https://avatars2.githubusercontent.com/u/11801943?s=460&v=4">
+                                <h3>Sam Faulkner</h3>
+                                <div style={{
+                                    textAlign: 'left'
+                                }}>
+                                    <p><strong>Bio:</strong> </p>
+                                    <p><strong>Responsibilities:</strong> </p>
+                                    <p><strong>Commits: </strong>{this.state['therealsamf'][0]}</p>
+                                    <p><strong>Issues: </strong>{this.state['therealsamf'][1]}</p>
+                                    <p><strong>Unit Tests: </strong>{this.state['therealsamf'][2]}</p>
+                                </div>
+                            </Thumbnail>
                         </Col>
                     </Row>
                     <Row>
-                        <Col xs={12} md={4}>
-                            <Image></Image>
+                        <Col md={4}>
+                            <Thumbnail src="">
+                                <h3>Mitchell Marshe</h3>
+                                <div style={{
+                                    textAlign: 'left'
+                                }}>
+                                    <p><strong>Bio:</strong> </p>
+                                    <p><strong>Responsibilities:</strong> </p>
+                                    <p><strong>Commits: </strong>{this.state['mitchellmarshe'][0]}</p>
+                                    <p><strong>Issues: </strong>{this.state['mitchellmarshe'][1]}</p>
+                                    <p><strong>Unit Tests: </strong>{this.state['mitchellmarshe'][2]}</p>
+                                </div>
+                            </Thumbnail>
                         </Col>
-                        <Col xs={12} md={4}>
-                            <Image></Image>
+                        <Col md={4}>
+                            <Thumbnail src="https://avatars0.githubusercontent.com/u/21695410?s=460&v=4">
+                                <h3>Andrew Rieder</h3>
+                                <div style={{
+                                    textAlign: 'left'
+                                }}>
+                                    <p><strong>Bio:</strong> </p>
+                                    <p><strong>Responsibilities:</strong> </p>
+                                    <p><strong>Commits: </strong>{this.state['atrieder'][0]}</p>
+                                    <p><strong>Issues: </strong>{this.state['atrieder'][1]}</p>
+                                    <p><strong>Unit Tests: </strong>{this.state['atrieder'][2]}</p>
+                                </div>
+                            </Thumbnail>
                         </Col>
-                        <Col xs={12} md={4}>
-                            <Image></Image>
+                        <Col md={4}>
+                            <Thumbnail src="">
+                                <h3>Ekin Soysal</h3>
+                                <div style={{
+                                    textAlign: 'left'
+                                }}>
+                                    <p><strong>Bio:</strong> </p>
+                                    <p><strong>Responsibilities:</strong> </p>
+                                    <p><strong>Commits: </strong>{this.state['esoysal'][0]}</p>
+                                    <p><strong>Issues: </strong>{this.state['esoysal'][1]}</p>
+                                    <p><strong>Unit Tests: </strong>{this.state['esoysal'][2]}</p>
+                                </div>
+                            </Thumbnail>
                         </Col>
                     </Row>
                 </Grid>
-                <br/>
+                <hr/>
                 {/*Total Stats*/}
                 <h3><strong>Team Stats</strong></h3>
-                <p><strong>Commits: </strong></p>
-                <p><strong>Issues: </strong></p>
-                <p><strong>Unit Tests: </strong></p>
+                <p><strong>Commits: </strong>{this.state.total[0]}</p>
+                <p><strong>Issues: </strong>{this.state.total[1]}</p>
+                <p><strong>Unit Tests: </strong>{this.state.total[2]}</p>
                 {/*Data*/}
                 {/*Tools*/}
                 {/*GitBook*/}
