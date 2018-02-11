613c682503bda887d55f72502574e6644e7e5c1f
Sam Faulkner <srf.000@hotmail.com>
2018-02-11 17:35:22 -0600

    Added react-router links to the NavBar's navitems
    The anchors and NavItems in the Navbar would previously trigger the
    app being reloaded with each click. React-router's links allow us
    to redirect with no downtime in-between pages. Some extra
    dependencies were required for this though as react-bootstrap's
    NavItems had an anchor tag by default and didn't play nice with
    react-router, thus react-router-bootstrap was added.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,37 +6,77 @@ import React from 'react';
 import { Navbar, Nav, Brand, Toggle, NavItem, Forms, FormGroup, 
          FormControl, Button } from 'react-bootstrap';
 
+import { Link } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+
+/**
+ * @description - Function that generates a LinkContainer for the Navbar
+ * @param {String} href - the location to redirect
+ * @param {String} label - the text to display to the user. Eventually
+ * we'll probably pass in a component here that will accept some extra styling
+ * in order to customize what each NavItem looks like depending on the current 
+ * router
+ * @param {Object} linkProps - extra props (such as key) that would need to be given to the
+ * outlying LinkContainer component
+ * @param {Object} navProps - Extra props (such as eventKey) that need to be placed within the 
+ * NavItem
+ * @returns {React.Component}
+ */
+function createLinkContainerNavItem(href, label, linkProps, navProps) {
+	return (
+		<LinkContainer to={href} {...linkProps}>
+			<NavItem {...navProps}>
+				{label} {/* Just text for now, later on should be it's own component */}
+			</NavItem>
+		</LinkContainer>
+	);
+}
+
+const pages = [
+	{
+		href: '/games',
+		label: 'Games'
+	},
+	{
+		href: '/developers',
+		label: 'Developers'
+	},
+	{
+		href: '/articles',
+		label: 'Articles'
+	},
+	{
+		href: '/about',
+		label: 'About'
+	}
+];
+
 class NavBar extends React.Component {
 	render() {
 		return (
 			<Navbar inverse collapseOnSelect>
 			  <Navbar.Header>
 			    <Navbar.Brand>
-			      <a href="/">GameFrame.Online</a>
+			      <Link to="/">GameFrame.Online</Link>
 			    </Navbar.Brand>
 			    <Navbar.Toggle />
 			  </Navbar.Header>
 			  <Navbar.Collapse>
 			    <Nav>
-			      <NavItem eventKey={1} href="/games">
-			        Games
-			      </NavItem>
-			      <NavItem eventKey={2} href="/developers">
-			        Developers
-			      </NavItem>
-				  <NavItem eventKey={3} href="/articles">
-			        Articles
-			      </NavItem>
-				  <NavItem eventKey={4} href="/about">
-			        About
-			      </NavItem>
-				</Nav>
-				<Navbar.Form pullRight>
-				  <FormGroup>
-				    <FormControl type="text" placeholder="Search" />
-			      </FormGroup>{' '}
+			    	{pages.map((page, index) => {
+			    		return createLinkContainerNavItem(page.href, page.label,
+			    			{key: index},
+			    			{eventKey: index}
+		    			);
+			    	})}
+					</Nav>
+					<Navbar.Form pullRight>
+					  <FormGroup>
+					    <FormControl type="text" placeholder="Search" />
+			      </FormGroup>
+			      {' '}
 			      <Button type="submit">Submit</Button>
-				</Navbar.Form>
+					</Navbar.Form>
 			  </Navbar.Collapse>
 			</Navbar>
 		);
