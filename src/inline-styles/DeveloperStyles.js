4f6c9ab778ebb86312150ad5b43c8f04de9c45c3
Andrew Rieder <atrieder@me.com>
2018-02-13 19:48:22 -0600

    Forgot to add DeveloperStyles (It's here now though)

---
+++ src/inline-styles/DeveloperStyles.js
@@ -0,0 +1,61 @@
+
+const Developer = {
+  border: {
+    padding: '0 2% 2% 2%',
+  },
+  name: {
+    paddingLeft: '5%'
+  },
+  secondaryInfo: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  imageContainer: {
+    maxWidth: '33.333333%',
+    overflow: 'hidden',
+    transition: 'transform 0.2s',
+    transform: 'none',
+    zIndex: '1',
+  },
+  imageContainerHover: {
+    overflow: 'visible',
+    transform: 'scale(1.1)',
+    zIndex: '100'
+  },
+  image: {
+    maxWidth: '100%',
+    maxHeight: '100%',
+    transition: 'box-shadow 0.3s',
+    boxShadow: 'none',
+  },
+  imageHover: {
+    boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)'
+  },
+  imageGallery: {
+    display: 'flex',
+    overflow: 'visible',
+  },
+  about: {
+    padding: '20px 2% 20px 2%'
+  },
+  developer: {
+    display: 'flex',
+    justifyContent: 'space-around'
+  },
+  articles: {
+    paddingLeft: '2%'
+  },
+  twitter: {
+    paddingLeft: '2%'
+  },
+  games: {
+    paddingLeft: '2%'
+  },
+  jumboTron: {
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: '10px'
+  }
+}
+
+export { Developer };
