53a4951dc5753be58d8fb32a41c023970126c523
Andrew Rieder <atrieder@me.com>
2018-02-14 00:15:44 -0600

    Added remaining article images.

--- src/components/Article4.js
+++ src/components/Article4.js
@@ -13,8 +13,8 @@ class Article4 extends React.Component {
 		return (
 			<div>
 				<Article title='Rocket League Roadmap: Spring 2018' 
-					     author='DIRKENED'
-						 publish='January 31, 2017'
+					     author='Corey Davis'
+						 publish='January 31, 2018'
 						 thumbnailURL='https://i.imgur.com/bR8e9SE.jpg'
 						 summary='Hi everybody! With our next update fast approaching, we wanted to share with you some of our plans for the first few months of 2018. 
 						          Were testing out a new update schedule this year that alternates Content Updates - which include things like new Competitive Seasons 
@@ -22,6 +22,8 @@ class Article4 extends React.Component {
 						 articleURL='http://steamcommunity.com/games/252950/announcements/detail/1675771900733255728' article='Click Here!'
 						 gameURL='/RocketLeague' game='Rocket League'
 						 developerURL='/Psyonix' developer='Psyonix'
+						publisher='Psyonix'
+						image='https://rocketleague.media.zestyio.com/rl_generic_screen_5_18_1920x1080.c6e1dc555a6eff57c623d9877706c9a5.jpg'
 				/>
 			</div>
 		);
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -19,8 +19,8 @@ class Articles extends React.Component {
 				}}>
 					<TextCard url='/Article1' cover='' company='Brendan Caldwell'  year='January 10, 2018' title='Bridge Constructor Portal isnt a rollercoaster of laughs, but its still good' companyURL='/AlexNavarro'/>
 					<TextCard url='/Article2' cover='' company='Giant Bomb Staff' year='Giant Bomb Staff' title='Will Smiths Top 7 Games of 2017' companyURL='/GiantBombStaff' />
-					<TextCard url='/Article3' cover='' company='DISHMINK' year='February 1, 2017' title='V0.73 and release plans' companyURL='DISHMINK' />
-					<TextCard url='/Article4' cover='https://i.imgur.com/bR8e9SE.jpg' company='DIRKENED' year='January 31, 2017' title='Rocket League Roadmap: Spring 2018' companyURL='/DIRKENED' />
+					<TextCard url='/Article3' cover='' company='Dominic Tarason' year='February 2, 2018' title='The Forest launches this April, after four years lost in the woods' companyURL='DISHMINK' />
+					<TextCard url='/Article4' cover='https://i.imgur.com/bR8e9SE.jpg' company='Corey Davis' year='January 31, 2018' title='Rocket League Roadmap: Spring 2018' companyURL='/DIRKENED' />
 				</div>
 			</div>
     );
