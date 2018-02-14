b0cf5aa2b138e257daa761a33e8df16f40e57397
Andrew Rieder <atrieder@me.com>
2018-02-14 00:06:13 -0600

    Added image to article 2.

--- src/components/Article2.js
+++ src/components/Article2.js
@@ -16,11 +16,12 @@ class Article2 extends React.Component {
 					     author='Giant Bomb Staff'
 						 publish='December 26, 2017'
 						 thumbnailURL=''
-						 summary='It turns out, I really enjoy hunting people for sport. At a time when I thought I was officially done 
-						          playing competitive online shooters, PlayerUnknowns Battlegrounds arrived and proved me wrong.'
+						 summary={['It turns out, I really enjoy hunting people for sport. At a time when I thought I was officially done playing competitive online shooters, PlayerUnknowns Battlegrounds arrived and proved me wrong. <a href=\"https:\/\/www.giantbomb.com\/articles\/will-smiths-top-7-games-of-2017\/1100-5693\/\">Click here!</a>']}
 						 articleURL='https://www.giantbomb.com/articles/will-smiths-top-7-games-of-2017/1100-5693/' article='Click Here!'
 						 gameURL='/PLAYERUNKNOWNSBATTLEGROUNDS' game='PLAYERUNKNOWNS BATTLEGROUNDS'
 						 developerURL='/PUBGCorp' developer='PUBG Corp'
+						publisher='Giant Bomb'
+						image='https://static.giantbomb.com/uploads/original/0/39/2985782-maxresdefault+%2833%29.jpg'
 				/>
 			</div>
 		);
