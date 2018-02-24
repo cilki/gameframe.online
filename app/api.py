b0841ee637c0e85c4ebae16e1d1f10294d165d4d
Tyler Cook <tylercurtiscook@gmail.com>
2018-02-23 19:23:51 -0600

    Add API endpoints

---
+++ app/api.py
@@ -0,0 +1,21 @@
+import flask
+import flask_sqlalchemy
+import flask_restless
+
+from orm import Game, Developer, Article, Tweet, Stream, Video
+
+def generate_api(app, db):
+    """
+    Generate the API endpoints.
+    """
+
+    # Create the API manager
+    api_manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
+
+    # Generate the API endpoints
+    api_manager.create_api(Game, methods=['GET'], url_prefix='/get')
+    api_manager.create_api(Developer, methods=['GET'], url_prefix='/get')
+    api_manager.create_api(Article, methods=['GET'], url_prefix='/get')
+    api_manager.create_api(Tweet, methods=['GET'], url_prefix='/get')
+    api_manager.create_api(Stream, methods=['GET'], url_prefix='/get')
+    api_manager.create_api(Video, methods=['GET'], url_prefix='/get')
