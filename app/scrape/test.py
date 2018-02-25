b6bcecf3aa1a54532f603cc0841d4d2d8c0fb6c8
Tyler Cook <tylercurtiscook@gmail.com>
2018-02-25 11:33:38 -0600

    Add unit tests for `get_game`

--- app/scrape/test.py
+++ app/scrape/test.py
@@ -4,10 +4,27 @@
 # --------------------------------
 
 from unittest import main, TestCase
+from igdb import get_game, rq_game, CACHE_GAME_IGDB
+from steam import get_game, rq_game, CACHE_GAME_STEAM
 import pymysql
 
 
-class TestScraper (TestCase):
+class TestScrape (TestCase):
+
+    def test_get_game_steam(self):
+        # Test value
+        self.assertEqual("NightSky", get_game(
+            {'appid': '99700', 'name': ''})['99700']['data']['name'])
+
+        # Test cache
+        self.assertTrue(os.path.isfile("%s/%d" % (CACHE_GAME_STEAM, 99700)))
+
+    def test_get_game_igdb(self):
+        # Test value
+        self.assertEqual("MX Superfly", get_game(4024)[0]['name'])
+
+        # Test cache
+        self.assertTrue(os.path.isfile("%s/%d" % (CACHE_GAME_IGDB, 4024)))
 
     def test_db_add_entity_steam(self):
         pass
