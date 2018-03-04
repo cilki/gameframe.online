db66df65aee869892bf5198400833a668d7ae5f0
Andrew Rieder <atrieder@me.com>
2018-03-04 15:30:44 -0800

    Saving progress on new navigation bar, but it has a bug.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,11 @@
  */
 
 import React from 'react';
+import Radium from 'radium';
 import {
   Navbar, Nav, Brand, Toggle, NavItem, Forms, FormGroup,
-  FormControl, Button, Image, Glyphicon
+  FormControl, Button, Image, Glyphicon, Tooltip, OverlayTrigger,
+  Dropdown
 } from 'react-bootstrap';
 
 import { Link } from 'react-router-dom';
@@ -27,12 +29,41 @@ import { LinkContainer } from 'react-router-bootstrap';
  */
 function createLinkContainerNavItem(href, imageSrc, label, linkProps, navProps) {
   return (
-    <LinkContainer to={href} {...linkProps}>
-      <NavItem {...navProps}>
-  		  <img style={{maxWidth: '100%', maxHeight: '100%'}} src={imageSrc} />
-            {label} {/* Just text for now, later on should be it's own component */}
-      </NavItem>
-    </LinkContainer>
+    <div style={{
+      display: 'flex',
+        padding: '4px 12% 4px 12%',
+      }}
+    >
+      <LinkContainer to={href} {...linkProps} style={{
+        }}
+      >
+        <div style={{
+          
+          }}
+        >
+          <NavItem {...navProps} style={{
+            }}
+          >
+            <OverlayTrigger placement='bottom' overlay={
+              <Tooltip id="tooltip">
+                {label}
+              </Tooltip>
+            }>
+  		    <img style={{
+              position: 'relative',
+              margin: 'auto',
+              display: 'block',
+              minWidth: '16px',
+              minHeight: '16px',
+              maxWidth: '32px',
+              maxHeight: '32px',
+              filter: 'saturate(0%) invert(100%)',
+              }} src={imageSrc}
+            /></OverlayTrigger>
+          </NavItem>
+        </div>
+      </LinkContainer>
+    </div>
   );
 }
 
@@ -65,31 +96,91 @@ const pages = [
 class NavBar extends React.Component {
   render() {
     return (
-      <Navbar inverse collapseOnSelect >
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">GameFrame.Online</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignContent: 'center',
+        flexWrap: 'nowrap',
+        background: 'linear-gradient(black, #222)',
+        verticalAlign:'middle',
+        }}
+      >
+        <Link to="/" className='navbar-toggle' data-toggle='collapse'>
+          <img style={{
+            width: '30px',
+            height: '24px',
+            padding: '0px 4px 0px 4px',
+            alignItem: 'center'
+            }}
+            src='../../static/images/mininavbarlogo.png'
+            alt='GameFrame.Online'
+          />
+        </Link>
         <Navbar.Collapse>
-          <Nav pullLeft={true}>
-            {/* This dynamically creates all of the NavItems for us depending on the objects in `pages` */}
-            {pages.map((page, index) => createLinkContainerNavItem(page.href, page.imageSrc, page.label, { key: page.href }, { eventKey: index }, ))}
-          </Nav>
-          <Navbar.Form pullRight>
-            <FormGroup>
-              <FormControl type="text" placeholder="Search" />
-  		        <Button>
-  		          <Glyphicon glyph="search" />
-  		        </Button>
-            </FormGroup>
-            {' '}
-          </Navbar.Form>
+          <div style={{
+            }}
+          >
+            <Link to="/">
+              <img style={{
+                width: '148px',
+                height: '40px',
+                padding: '4px 4px 4px 4px',
+                alignItem: 'center'
+                }}
+                src='../../static/images/navbarlogo.png'
+                alt='GameFrame.Online'
+              />
+            </Link>
+          </div>
+        </Navbar.Collapse>
+        <div style={{
+            alignSelf: 'flex-start',
+            flexGrow: '1'
+          }}
+        >
+          <Navbar.Collapse>
+            <Nav pullLeft={true}>
+              <div style={{
+                display: 'flex',
+                flexDirection: 'row',
+                maxWidth: '100%',
+                alignItem: 'center'
+                }}
+              >
+                {/* This dynamically creates all of the NavItems for us depending on the objects in `pages` */}
+                {pages.map((page, index) => createLinkContainerNavItem(page.href, page.imageSrc, page.label, { key: page.href }, { eventKey: index }, ))}
+              </div>
+            </Nav>
           </Navbar.Collapse>
-      </Navbar>
+        </div>
+        <div style={{}}>
+        <img className='navbar-toggle' data-toggle='collapse'
+          style={{
+          width: '30px',
+          height: '24px',
+          padding: '0px 4px 0px 4px',
+          alignItem: 'flex-end'
+          }}
+          src='../../static/images/mininavbarlogo.png'
+          alt='GameFrame.Online'
+        />
+          <Navbar.Collapse>
+            <Navbar.Form>
+              <FormGroup>
+                <FormControl type="text" placeholder="Search" />
+                <Button>
+                  <Glyphicon glyph="search" />
+                </Button>
+              </FormGroup>
+              {' '}
+            </Navbar.Form>
+          </Navbar.Collapse>
+        </div>
+
+      </div>
     );
   }
 }
 
-export default NavBar;
+export default Radium(NavBar);
