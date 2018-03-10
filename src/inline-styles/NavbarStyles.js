44960ed29184ff6ba337fd4cf6ce467a8d873efd
Andrew Rieder <atrieder@me.com>
2018-03-10 14:47:13 -0600

    Fixed hover detection for navbar buttons.

--- src/inline-styles/NavbarStyles.js
+++ src/inline-styles/NavbarStyles.js
@@ -2,6 +2,9 @@
 const Navbar = {
     itemMain: {
       color: '#f9f9f9',
+      transition: 'color 3.0s ease-out, background-color 0.3s ease-out, invert 0.3s ease-out',
+      padding: '15px',
+      margin: '-15px',
       ':hover': {
         backgroundColor: '#595959',
         color: '#f9f9f9',
@@ -9,7 +12,7 @@ const Navbar = {
         padding: '15px',
         margin: '-15px',
         filter: 'invert(100%)',
-        transition: 'color 1.0s ease, background-color 0.5s ease, invert 0.5s ease'
+        transition: 'color 3.0s ease-in, background-color 0.3s ease-in, invert 0.3s ease-in'
       }
     },
     navItem: {
