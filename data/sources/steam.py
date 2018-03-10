2622d331594beea3e04999e0f392805243b0f215
Tyler Cook <tylercurtiscook@gmail.com>
2018-03-10 10:15:07 -0600

    Improve modularity and speed up merging

--- data/sources/steam.py
+++ data/sources/steam.py
@@ -9,6 +9,7 @@ import sys
 
 from codecs import open
 from functools import lru_cache
+from tqdm import tqdm
 
 import requests
 from ratelimit import rate_limited
@@ -40,7 +41,7 @@ def rq_app_list():
 @rate_limited(period=35, every=60)
 def rq_game(app):
     """
-    Request game metadata using the store API
+    Request game metadata using the store API.
     """
     print("[STEAM] Downloading game metadata for: %s" %
           app['name'].encode('utf-8', 'ignore'))
@@ -52,28 +53,32 @@ def rq_game(app):
     return rq.json()
 
 
-def get_game(app):
+def load_game(app):
     """
-    Retrieve a game from the cache or download it from Steam.
+    Retrieve a filtered game from the cache or download it from Steam.
     """
     assert os.path.isdir(CACHE_GAME_STEAM)
+    assert 'appid' in app
+    assert 'name' in app
+    appid = app['appid']
 
-    if not os.path.isfile("%s/%d" % (CACHE_GAME_STEAM, app['appid'])):
+    if not os.path.isfile("%s/%d" % (CACHE_GAME_STEAM, appid)):
         game = rq_game(app)
 
         # Write to the cache
-        with open("%s/%d" % (CACHE_GAME_STEAM, app['appid']), 'w', 'utf8') as h:
+        with open("%s/%d" % (CACHE_GAME_STEAM, appid), 'w', 'utf8') as h:
             h.write(json.dumps(game, ensure_ascii=False))
     else:
-        with open("%s/%d" % (CACHE_GAME_STEAM, app['appid']), 'r', 'utf8') as h:
+        with open("%s/%d" % (CACHE_GAME_STEAM, appid), 'r', 'utf8') as h:
             game = json.load(h)
 
-    game = game[str(app['appid'])]
+    game = game[str(appid)]
 
     # Filter failed queries
     if game['success'] == 'false':
         return None
 
+    # Ensure the game has data
     if 'data' not in game:
         return None
 
@@ -83,114 +88,124 @@ def get_game(app):
     if not game['type'] == 'game':
         return None
 
+    # Ensure the game has a name
+    if 'name' not in game:
+        return None
+
+    # TODO Apply more filtering
     return game
 
 
+def find_game(appid, name):
+    """
+    Attempt to locate a game in the database
+    """
+    # ID matching
+    game = Game.query.filter_by(steam_id=appid).first()
+
+    # Name matching
+#    if game is None:
+#        # TODO add tolerance
+#        game = Game.query.filter_by(
+#            name=name.encode('utf-8', 'ignore')).first()
+
+    # TODO Provide more robust searching with game_json
+    if game is None:
+        pass
+
+    # Construct new Game
+    if game is None:
+        game = Game()
+
+    return game
+
+
+def build_game(game_json):
+    """
+    Build a Game object from the raw data, taking into account previous Games.
+    """
+    game = find_game(game_json['steam_appid'], game_json['name'])
+
+    # Steam ID
+    game.steam_id = game_json['steam_appid']
+
+    # Title
+    if game.name is None:
+        game.name = game_json['name']
+
+    # Summary
+    if game.summary is None and 'detailed_description' in game_json:
+        game.summary = game_json['detailed_description']
+
+    # Price
+    if game.price is None and 'price_overview' in game_json:
+        game.price = game_json['price_overview']['initial']
+
+        # TODO Discounts
+
+    # Website
+    if game.website is None and 'website' in game_json:
+        game.website = game_json['website']
+
+    # Platforms
+    if 'platforms' in game_json:
+        game.platform_win = game_json['platforms']['windows']
+        game.platform_lin = game_json['platforms']['linux']
+        game.platform_mac = game_json['platforms']['mac']
+
+    # Screenshots
+    if 'screenshots' in game_json:
+        for screenshot in game_json['screenshots']:
+            game.screenshots = append_csv(
+                game.screenshots, screenshot['path_full'])
+
+    # Genres
+    if 'genres' in game_json:
+        for genre in game_json['genres']:
+            game.genres = append_csv(game.genres, genre['description'])
+
+    # Release date
+    if game.release is None and 'release_date' in game_json:
+        if game_json['release_date']['date']:
+            game.release = parse_steam_date(
+                game_json['release_date']['date'])
+
+    return game
+
+
+@lru_cache(maxsize=1)
 def gather_games():
     """
-    Download any games missing from the cache.
+    Build a filtered list of Game objects.
     """
     apps = rq_app_list()
 
     print("[STEAM] Gathering games")
 
-    i = 0
-    for app in apps:
-        if not get_game(app) is None:
-            i += 1
+    games = []
 
-    print("[STEAM] Gathered %d games" % i)
-
-
-def filter_games():
-    """
-    Return a filtered list of games.
-    """
-    apps = rq_app_list()
-
-    print("[STEAM] Filtering games")
-    filtered = []
-
-    for app in apps:
-        game_json = get_game(app)
-
+    # Load games
+    for app in tqdm(apps):
+        game_json = load_game(app)
         if game_json is None:
             continue
+        games.append(build_game(game_json))
 
-        if 'name' not in game_json:
-            continue
+    # TODO Perform mutual filtering
 
-        # TODO Apply more filtering
-        filtered += [app]
-
-    print("[STEAM] Filtered out %d games" % (len(apps) - len(filtered)))
-    return filtered
+    print("[STEAM] Gathered %d games" % len(games))
+    return games
 
 
 def merge_games(db):
     """
     Insert the filtered list of games into the database.
     """
+    games = gather_games()
 
-    i = 0
-    for app in filter_games():
-        game_json = get_game(app)
-        assert 'name' in game_json
-
-        # TODO Find the same game in database if it already exists
-        if True:
-            game = Game()
-            i += 1
-        else:
-            game = Game()
-
-        game.steam_id = int(game_json['steam_appid'])
-
-        # Title
-        if game.name is None:
-            game.name = game_json['name']
-
-        # Summary
-        if game.summary is None and 'detailed_description' in game_json:
-            game.summary = game_json['detailed_description']
-
-        # Price
-        if game.price is None and 'price_overview' in game_json:
-            game.price = game_json['price_overview']['initial']
-
-            # TODO Discounts
-
-        # Website
-        if game.website is None and 'website' in game_json:
-            game.website = game_json['website']
-
-        # Platforms
-        if 'platforms' in game_json:
-            game.platform_win = game_json['platforms']['windows']
-            game.platform_lin = game_json['platforms']['linux']
-            game.platform_mac = game_json['platforms']['mac']
-
-        # Screenshots
-        if 'screenshots' in game_json:
-            for screenshot in game_json['screenshots']:
-                game.screenshots = append_csv(
-                    game.screenshots, screenshot['path_full'])
-
-        # Genres
-        if 'genres' in game_json:
-            for genre in game_json['genres']:
-                game.genres = append_csv(game.genres, genre['description'])
-
-        # Release date
-        if game.release is None and 'release_date' in game_json:
-            if game_json['release_date']['date']:
-                game.release = parse_steam_date(
-                    game_json['release_date']['date'])
-
-        print("Uploading game (%d): %s" %
-              (game.steam_id, game.name.encode('utf-8', 'ignore')))
-
+    print("[STEAM] Merging games")
+    for game in games:
         db.session.add(game)
-        db.session.commit()
+    db.session.commit()
 
-    print("[STEAM] Inserted %d new games" % i)
+    print("[STEAM] Merge complete")
