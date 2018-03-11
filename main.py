dcbd7b714e1008279fe4eeecaa5c4bf84789aed3
Tyler Cook <tylercurtiscook@gmail.com>
2018-03-10 19:37:47 -0600

    Remove frontend route

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import os
-from flask import Flask, render_template
+# --------------------------------
+# Flask Backend Entry-point      -
+# Copyright (C) 2018 GameFrame   -
+# --------------------------------
 
-# Setup Flask
+from app.orm import db
+from app.api import generate_api
+from flask import Flask
+import os
+
+# Initialize Flask
 app = Flask(__name__)
 
 # Configure SQLAlchemy
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_URI']
 
 # Initialize database
-from app.orm import db
 db.init_app(app)
 
 # Initialize API
-from app.api import generate_api
 generate_api(app, db)
 
-
-@app.route("/", defaults={'path': ''})
-@app.route("/<path:path>")
-def index(path):
-    return render_template('index.html')
-
-
+# Start Flask
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80)
+    app.run(host=os.environ['BIND_ADDRESS'], port=80)
