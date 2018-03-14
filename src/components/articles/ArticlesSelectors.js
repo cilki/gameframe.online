1052932327f793a9ac05f31f10a1b159b0da4c05
longhorn396 <devin@drawhorn.com>
2018-03-14 10:58:40 -0500

    Following games template for ArticlesSelectors

--- src/components/articles/ArticlesSelectors.js
+++ src/components/articles/ArticlesSelectors.js
@@ -3,6 +3,38 @@
  * Selectors for the state tree for Articles.js
  */
 
-export { //eslint-disable-line
+ /**
+  * @description - Input selector for the articles
+  * 'requested' state
+  * @param {Map} state
+  * @returns {Boolean}
+  */
+function getArticlesRequested(state){
+    return state.articles.articlesRequested;
+}
 
+ /**
+  * @description - Input selector for the articles
+  * @param {Map} state
+  * @returns {Boolean}
+  */
+function getArticles(state){
+    const articles = state.articles.articles.toJS();
+    return articles;
+}
+
+/**
+ * @description - Trivial selector for finding the 
+ * error state of the articles
+ * @param {Map} state
+ * @returns {String|null}
+ */
+function getArticlesError(state) {
+    return state.articles.articlesError;
+}
+
+export { //eslint-disable-line
+    getArticlesRequested,
+    getArticles,
+    getArticlesError,
 };
