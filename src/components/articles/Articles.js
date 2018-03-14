1bf877c5bf4ae036dd944d8d9ab85f4b20e2769e
longhorn396 <devin@drawhorn.com>
2018-03-14 11:47:44 -0500

    Following games template for Articles; mostly complete

--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -3,17 +3,30 @@
  */
 
 import React from 'react';
-// import PropTypes from 'prop-types';
-// import Radium from 'radium';
+import PropTypes from 'prop-types';
+import Radium from 'radium';
+
+import CommonAssets from '../../inline-styles/CommonAssets';
+import Styles from './ArticlesStyles';
+import TextCard from '../TextCard';
+
 // import TextCard from '../TextCard';
 
 class Articles extends React.Component {
   static propTypes = {
+    articles: PropTypes.arrayOf(PropTypes.shape({
 
+    })),
+    articlesError: PropTypes.string, //eslint-disable-line
+    articlesRequested: PropTypes.bool, //eslint-disable-line
+
+    fetchArticles: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-
+    articles: [],
+    articlesError: null,
+    articlesRequested: false,
   };
 
   constructor(props) {
@@ -25,26 +38,36 @@ class Articles extends React.Component {
    * @description - React lifecycle method used to fetch the data
    */
   componentDidMount() {
-
+    this.props.fetchArticles();
   }
 
   render() {
     return (
       <div>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'row',
-          flexWrap: 'wrap',
-          position: 'relative',
-          justifyContent: 'space-around',
-          maxWidth: '100%',
-        }}
-        >
-          {/* Dynamic content for Articles goes here! */}
+        <div style={[
+          CommonAssets.stripeOverlay,
+          // CommonAssets.backgroundColor,
+          CommonAssets.fillBackground,
+        ]}
+        />
+        <div style={[Styles.grid]}>
+          {
+            this.props.articles.map((article) => {
+              return (
+                <TextCard 
+                  key={article.article_id}
+                  company={article.company}
+                  title={article.title}
+                  url={'articles/${article.article_id}'}
+                  year={article.year}
+                />
+              );
+            })
+          }
         </div>
       </div>
     );
   }
 }
 
-export default Articles;
+export default Radium(Articles);
---
+++ src/components/articles/ArticlesStyles.js
@@ -0,0 +1,17 @@
+
+/**
+ * Inline stylesheet for Articles
+ */
+
+export default {
+    grid: {
+      display: 'flex',
+      flexDirection: 'row',
+      flexWrap: 'wrap',
+      position: 'relative',
+      justifyContent: 'space-around',
+      maxWidth: '100%',
+      paddingTop: '2%',
+    },
+  };
+  
