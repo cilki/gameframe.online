24e131c1f4d528372de9c114e4edef88db199b65
longhorn396 <devin@drawhorn.com>
2018-03-16 17:11:01 -0500

    Refactored page for single developer

--- src/components/developer/Developer.js
+++ src/components/developer/Developer.js
@@ -11,118 +11,189 @@ import { Link } from 'react-router-dom';
 import DeveloperStyles from './DeveloperStyles';
 import InstanceDetails from '../InstanceDetails';
 
+import CommonAssets from '../../inline-styles/CommonAssets';
+
 /**
- * @description - Creates links for a developer
+ * @description - Helper method for generating a component
+ * to hold a screenshot
+ * @param {Object} props
+ * @param {String} props.src
+ * @param {String} props.alt
+ * @returns {React.Component}
+ */
+function screenshot({ url, alt }) {
+  const src = url.indexOf('http') >= 0 ? url : `https://${url}`;
+  return (
+    <div
+      key={`${url}-div-image`}
+      style={[DeveloperStyles.imageContainer]}
+    >
+      <div>
+        <a href={src}>
+          <img
+            key={`${url}-image`}
+            src={src}
+            alt={alt}
+            style={[DeveloperStyles.image]}
+          />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+screenshot.propTypes = {
+  url: PropTypes.string.isRequired,
+  alt: PropTypes.string,
+};
+
+screenshot.defaultProps = {
+  alt: '',
+};
+
+/**
+ * @description - Helper method for rendering a link to a game or article
  * @param {Object} props
  * @param {String} url
  * @param {String} name
  * @returns {React.Component}
  */
-function link({ url, name }) {
+function link({ label, url, key }) {
   return (
-    <Link
-      to={url}
-      style={{
-        textDecoration: 'none',
-      }}
-    >
-      <Label>{name}</Label>
+    <Link key={key} to={url} style={{ textDecoration: 'none' }}>
+      <Label>{label}</Label>
     </Link>
   );
 }
 
 link.propTypes = {
-  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
+  key: PropTypes.oneOf([PropTypes.string, PropTypes.number]).isRequired,
 };
 
 /**
- * @description - Creates and returns a react component for
- * a single developer's instance page
+ * @description - Returns the main component to render a developer's own
+ * page
  * @param {Object} props
  * @returns {React.Component}
  */
-function Developer(props) {
-  return (
-    <InstanceDetails
-      style={{
-        container: undefined,
-        border: DeveloperStyles.border,
-        jumboTron: DeveloperStyles.jumboTron,
-      }}
-      twitterWidget={props.twitter}
-    >
-      <h1
-        style={[DeveloperStyles.name]}
-      >
-        {props.developer}
-        <div
-          style={[DeveloperStyles.logo]}
+class Developer extends React.Component {
+  static propTypes = {
+    // this is derived state using selectors
+    articles: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })),
+    name: PropTypes.string.isRequired,
+    // this is derived state using selectors
+    games: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+    })),
+    country: PropTypes.string,
+    logoURL: PropTypes.string,
+    twitter: PropTypes.object, //eslint-disable-line
+    foundation: PropTypes.string,
+    website: PropTypes.string,
+    // we currently don't use these but we may in the future
+    error: PropTypes.string, //eslint-disable-line
+    requested: PropTypes.bool, //eslint-disable-line
+
+    fetchDeveloper: PropTypes.func.isRequired,
+  };
+
+  static defaultProps = {
+    articles: [],
+    name: '',
+    games: [],
+    country: '',
+    logoURL: '',
+    twitter: null,
+    foundation: '',
+    website: '',
+    error: null,
+    requested: false,
+  };
+
+  /**
+   * @constructor
+   */
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    this.props.fetchDeveloper();
+  }
+
+  render() {
+    return (
+      <div>
+        <div style={[
+            CommonAssets.stripeOverlay,
+            CommonAssets.fillBackground,
+          ]}
+        />
+        <InstanceDetails
+          style={{
+            container: undefined,
+            border: DeveloperStyles.border,
+            jumboTron: DeveloperStyles.jumboTron,
+          }}
+          twitterWidget={this.props.twitter}
         >
-          <img
-            style={{ maxWidth: '100%', maxHeight: '100%' }}
-            src={props.logoURL}
-            alt={`${props.developer} logo`}
-          />
-        </div>
-      </h1>
-      <div
-        style={[DeveloperStyles.secondaryInfo]}
-      >
-        <p>Established: {props.year}</p>
-        <p>Location: {props.loc}</p>
+          <div>
+            <h1 style={[DeveloperStyles.name]}>{this.props.name}
+              <div style={[DeveloperStyles.logo]}>
+                <img
+                  style={{ maxWidth: '100%', maxHeight: '100%' }}
+                  src={this.props.logoURL}
+                  alt={`${this.props.name} logo`}
+                />
+              </div>
+            </h1>
+            <div style={[DeveloperStyles.secondaryInfo]}>
+              <p>Established: {this.props.foundation}</p>
+              <p>Location: {this.props.country}</p>
+              <p>Website: <a href={this.props.website}>{this.props.website}</a></p>
+            </div>
+          </div>
+          <div style={[DeveloperStyles.games]}>
+            <h3>Games:</h3>
+            {
+              this.props.games.length > 0 &&
+              <p>
+                {
+                  this.props.games.map(game => link({
+                    label: game.name,
+                    url: `/games/${game.id}`,
+                    key: `game-${game.id}`,
+                  }))
+                }
+              </p>
+            }
+          </div>
+          <div style={[DeveloperStyles.articles]}>
+            <h3>Articles:</h3>
+            {
+              this.props.articles.length > 0 &&
+              <p>
+                {
+                  this.props.articles.map(article => link({
+                    label: article.title,
+                    url: `/articles/${article.id}`,
+                    key: `article-${article.id}`,
+                  }))
+                }
+              </p>
+            }
+          </div>
+        </InstanceDetails>
       </div>
-
-      <div
-        style={[DeveloperStyles.about]}
-      >
-        <p>{props.about}</p>
-      </div>
-      <div style={[DeveloperStyles.games]}>
-        <h3>Games:</h3>
-        <p>
-          {
-            props.games.map(_game => link(_game))
-          }
-        </p>
-      </div>
-      <div style={[DeveloperStyles.articles]}>
-        <h3>Articles:</h3>
-        <p>
-          {
-            props.articles.map(_article => link(_article))
-          }
-        </p>
-      </div>
-    </InstanceDetails>
-  );
+    );
+  }
 }
-
-Developer.propTypes = {
-  about: PropTypes.string,
-  articles: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  })),
-  developer: PropTypes.string.isRequired,
-  games: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  })),
-  loc: PropTypes.string,
-  logoURL: PropTypes.string,
-  twitter: PropTypes.object, //eslint-disable-line
-  year: PropTypes.number,
-};
-
-Developer.defaultProps = {
-  about: '',
-  articles: [],
-  games: [],
-  loc: '',
-  logoURL: null,
-  // if the year isn't defined then it defaults to this year
-  year: new Date().getFullYear(),
-};
 
 export default Radium(Developer);
