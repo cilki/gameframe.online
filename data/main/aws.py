ced611231091b1d209852d83224dd186cdc92618
Tyler Cook <tylercurtiscook@gmail.com>
2018-03-16 20:04:45 -0500

    Check for resource on S3 before uploading

--- data/main/aws.py
+++ data/main/aws.py
@@ -4,9 +4,15 @@
 # --------------------------------
 
 import boto3
+from functools import lru_cache
 
 
-S3 = boto3.client('s3')
+@lru_cache(maxsize=1)
+def S3():
+    """
+    Create the AWS S3 client once
+    """
+    return boto3.client('s3')
 
 
 def upload_image(image, target):
@@ -14,7 +20,12 @@ def upload_image(image, target):
     Upload an image to S3 if required
     """
 
-    # TODO check for existence
+    # Check for existence
+    try:
+        S3().head_object(Bucket='gameframe', Key='image/' + target)
+        return
+    except ClientError: # TODO make error more specific if possible
+        pass
 
     # Upload
-    S3.upload_file(image, 'gameframe', 'image/cover/' + target)
+    S3().upload_file(image, 'gameframe', 'image/' + target)
