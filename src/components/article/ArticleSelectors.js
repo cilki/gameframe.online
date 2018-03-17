8493d147f5cf9315de3f3e30d145b029ffb683a4
longhorn396 <devin@drawhorn.com>
2018-03-17 13:07:18 -0500

    Rewriting ArticleSelectors in style of GameSelectors

--- src/components/article/ArticleSelectors.js
+++ src/components/article/ArticleSelectors.js
@@ -31,7 +31,57 @@ function makeGetArticle() {
   );
 }
 
+/**
+ * @description - Memoized selector for returning an article's developers
+ */
+function makeGetArticleDevelopers(_articleSelector = null) {
+  const articleSelector = _articleSelector === null ? makeGetArticle() : _articleSelector;
+  return createSelector(
+    [articleSelector, getDevelopers],
+    (article, developers) => {
+      const articleDevelopers = [];
+      if (article.developers) {
+        article.developers.forEach((developerId) => {
+          if (developerId >= 0 && developers[developerId]) {
+            articleDevelopers.push({
+              id: developerId,
+              name: developers[developerId] ? developers[developerId].name : null,
+            });
+          }
+        });
+      }
+      return articleDevelopers;
+    },
+  );
+}
+
+/**
+ * @description - Memoized selector for returning an article's game
+ */
+function makeGetArticleGames(_articleSelector = null) {
+  const articleSelector = _articleSelector === null ? makeGetArticle() : _articleSelector;
+  return createSelector(
+    [articleSelector, getGames],
+    (article, game) => {
+      const articleGames = [];
+      if(article.games) {
+        article.games.forEach((gameId) => {
+          if(gameId >= 0 && games[gameId]) {
+            articleGames.push({
+              id: gameId,
+              name: games[gameId] ? games[gameId].name : null,
+            });
+          }
+        });
+      }
+      return articleGames;
+    },
+  );
+}
+
 export {
   getArticle,
   makeGetArticle,
+  makeGetArticleDevelopers,
+  makeGetArticleGames,
 };
