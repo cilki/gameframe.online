048e9e38034434f681408381c927f90ffe38f8c4
Andrew Rieder <atrieder@me.com>
2018-03-21 18:27:56 -0500

    Changed how background overlay is implemented to improve performance.

--- src/inline-styles/CommonAssets.js
+++ src/inline-styles/CommonAssets.js
@@ -1,21 +1,17 @@
 const CommonAssets = {
   stripeOverlay: {
-    background: 'repeating-linear-gradient(rgba(39, 39, 39, 0.5), rgba(39, 39, 39, 0.5) 1px, rgba(52, 51, 52, 0.5) 1px, rgba(52, 51, 52, 0.5) 2px)',
-    WebkitTransform: 'translateZ(0)',
-    MozTransform: 'translateZ(0)',
-    MsTransform: 'translateZ(0)',
-    OTransform: 'translateZ(0)',
-    transform: 'translateZ(0)'
+    background: 'linear-gradient(rgba(39, 39, 39, 0.5), rgba(39, 39, 39, 0.5) 1px, rgba(52, 51, 52, 0.5) 1px, rgba(52, 51, 52, 0.5) 2px)',
+    backgroundSize: '1px 2px',
   },
 
   horizontalGradient: {
-    background: 'linear-gradient(to right, black, #090909, #191919, #191919, #090909, black)',
-    zIndex: '-101',
     WebkitTransform: 'translateZ(0)',
     MozTransform: 'translateZ(0)',
     MsTransform: 'translateZ(0)',
     OTransform: 'translateZ(0)',
-    transform: 'translateZ(0)'
+    transform: 'translateZ(0)',
+    background: 'linear-gradient(to right, black, #090909, #191919, #191919, #090909, black)',
+    zIndex: '-101',
   },
 
   backgroundColor: {
@@ -23,6 +19,11 @@ const CommonAssets = {
   },
 
   fillBackground: {
+    WebkitTransform: 'translateZ(0)',
+    MozTransform: 'translateZ(0)',
+    MsTransform: 'translateZ(0)',
+    OTransform: 'translateZ(0)',
+    transform: 'translateZ(0)',
     zIndex: '-100',
     position: 'fixed',
     top: '0px',
