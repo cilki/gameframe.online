ab05c37b69538dbda70dd8def9ba9e5acaf83565
Andrew Rieder <atrieder@me.com>
2018-03-20 20:57:08 -0500

    Added hardware acceleration/vendor prefixes to game and developer pages.

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -165,7 +165,9 @@ class Game extends React.Component {
     const screenshots = this.props.screenshots ? this.props.screenshots : [];
     const coverURL = this.props.cover && this.props.cover.indexOf('http') < 0 ? `https://${this.props.cover}` : this.props.cover;
     return (
-      <div>
+      <div style={[
+        GameStyles.main
+      ]}>
         <div style={[
             CommonAssets.stripeOverlay,
             CommonAssets.fillBackground,
