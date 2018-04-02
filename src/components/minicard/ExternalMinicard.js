8dee4ba22b0dea0bffe05acb1805377284624a7e
Andrew Rieder <atrieder@me.com>
2018-04-02 16:04:56 -0500

    Added alternative minicard type to handle external URLs.

---
+++ src/components/minicard/ExternalMinicard.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Radium from 'radium';
+import { Link } from 'react-router-dom';
+import MinicardStyles from './MinicardStyles';
+
+class ExternalMinicard extends React.Component {
+  static propTypes = {
+    label: PropTypes.any,//eslint-disable-line
+    url: PropTypes.string,
+    cover: PropTypes.string,
+    cardKey: PropTypes.string,
+  };
+
+  static defaultProps = {
+    label: 'Label Missing',
+    url: '',
+    cover: '../../../noImage.png',
+    cardKey: `${Math.random() * 512}`,
+  };
+
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    const label = this.props.label;
+    const url = this.props.url;
+    const cover = this.props.cover;
+    const cardKey = this.props.cardKey;
+    return (
+      <a key={cardKey} href={url} style={MinicardStyles.link}>
+        <div key={`${cardKey}-minicard`} style={[MinicardStyles.minicard(cover)]}>
+          <div style={[MinicardStyles.textArea]}>
+            <p style={[MinicardStyles.paragraph]}>
+              {label}
+            </p>
+          </div>
+        </div>
+      </a>
+    );
+  }
+}
+
+export default Radium(ExternalMinicard);
