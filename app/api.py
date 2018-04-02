ba87ec344983f577bfd18fd6f6263b824657ec37
Tyler Cook <tylercurtiscook@gmail.com>
2018-04-01 21:03:33 -0500

    Add optimized grid endpoints

--- app/api.py
+++ app/api.py
@@ -2,7 +2,7 @@ import flask
 import flask_sqlalchemy
 import flask_restless
 
-from .orm import Game, Developer, Article, Tweet, Video
+from .orm import Game, Developer, Article, Tweet, Video, Platform, Genre
 
 
 def generate_api(app, db):
@@ -13,9 +13,19 @@ def generate_api(app, db):
     # Create the API manager
     api_manager = flask_restless.APIManager(app, flask_sqlalchemy_db=db)
 
-    # Generate the API endpoints
+    # Generate root API endpoints
     api_manager.create_api(Game, methods=['GET'], url_prefix='/v1')
     api_manager.create_api(Developer, methods=['GET'], url_prefix='/v1')
     api_manager.create_api(Article, methods=['GET'], url_prefix='/v1')
     api_manager.create_api(Tweet, methods=['GET'], url_prefix='/v1')
     api_manager.create_api(Video, methods=['GET'], url_prefix='/v1')
+    api_manager.create_api(Platform, methods=['GET'], url_prefix='/v1')
+    api_manager.create_api(Genre, methods=['GET'], url_prefix='/v1')
+
+    # Generate grid API endpoints
+    api_manager.create_api(Game, methods=['GET'], url_prefix='/v1/grid',
+                           include_columns=['name', 'price', 'cover', 'platforms', 'release'])
+    api_manager.create_api(Developer, methods=['GET'], url_prefix='/v1/grid',
+                           include_columns=['logo', 'website', 'twitter', 'name'])
+    api_manager.create_api(Article, methods=['GET'], url_prefix='/v1/grid',
+                           include_columns=['title', 'cover', 'article_link'])
