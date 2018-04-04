b66324183c797da21ae4dc5331e834996f7ab347
Devin Drawhorn <devin@drawhorn.com>
2018-04-04 13:45:53 -0500

    Added searching through relations for all three models

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -87,10 +87,7 @@ class SearchResults extends React.Component {
 
   updateGameItems() {
     fetch(//eslint-disable-line
-      encodeURI(`http://api.gameframe.online/v1/grid/game?q={"filters":[{"or":[{"name":"c_name","op":"like","val":"%${this.state.query_string}%"},
-                                                                               {"name":"summary","op":"like","val":"%20${this.state.query_string}%20"},
-                                                                               {"name":"website","op":"like","val":"%${this.state.query_string}%"}]}],
-                                                             "order_by":[{"field":"metacritic","direction":"desc"}]}&results_per_page=1000`),
+      encodeURI(`http://api.gameframe.online/v1/grid/game?q={"filters":[{"or":[{"name":"c_name","op":"like","val":"%${this.state.query_string}%"},{"name":"summary","op":"like","val":"%20${this.state.query_string}%20"},{"name":"website","op":"like","val":"%${this.state.query_string}%"},{"name":"genres__name","op":"any","val":"${this.state.query_string}"},{"name":"platforms__name","op":"any","val":"${this.state.query_string}"},{"name":"developers__c_name","op":"any","val":"${this.state.query_string}"}]}],"order_by":[{"field":"metacritic","direction":"desc"}]}&results_per_page=1000`),
       { method: 'GET' },
     )
       .then(response => response.json())
@@ -101,10 +98,7 @@ class SearchResults extends React.Component {
 
   updateDeveloperItems() {
     fetch(//eslint-disable-line
-      encodeURI(`http://api.gameframe.online/v1/grid/developer?q={"filters":[{"or":[{"name":"name","op":"like","val":"%${this.state.query_string}%"},
-                                                                             {"name":"website","op":"like","val":"%${this.state.query_string}%"},
-                                                                             {"name":"twitter","op":"like","val":"%${this.state.query_string}%"}]}],
-                                                                  "order_by":[{"field":"name","direction":"asc"}]}&results_per_page=1000`),
+      encodeURI(`http://api.gameframe.online/v1/grid/developer?q={"filters":[{"or":[{"name":"name","op":"like","val":"%${this.state.query_string}%"},{"name":"website","op":"like","val":"%${this.state.query_string}%"},{"name":"twitter","op":"like","val":"%${this.state.query_string}%"},{"name":"games__c_name","op":"any","val":"${this.state.query_string}"}]}],"order_by":[{"field":"name","direction":"asc"}]}&results_per_page=1000`),
       { method: 'GET' },
     )
       .then(response => response.json())
@@ -115,11 +109,7 @@ class SearchResults extends React.Component {
 
   updateArticleItems() {
     fetch(//eslint-disable-line
-      encodeURI(`http://api.gameframe.online/v1/grid/article?q={"filters":[{"or":[{"name":"title","op":"like","val":"%${this.state.query_string}%"},
-                                                                                  {"name":"introduction","op":"like","val":"%20${this.state.query_string}%20"},
-                                                                                  {"name":"outlet","op":"like","val":"%${this.state.query_string}%"},
-                                                                                  {"name":"author","op":"like","val":"%${this.state.query_string}%"}]}],
-                                                                "order_by":[{"field":"title","direction":"asc"}]}&results_per_page=1000`),
+      encodeURI(`http://api.gameframe.online/v1/grid/article?q={"filters":[{"or":[{"name":"title","op":"like","val":"%${this.state.query_string}%"},{"name":"introduction","op":"like","val":"%20${this.state.query_string}%20"},{"name":"outlet","op":"like","val":"%${this.state.query_string}%"},{"name":"author","op":"like","val":"%${this.state.query_string}%"},{"name":"games__c_name","op":"any","val":"${this.state.query_string}"},{"name":"developers__c_name","op":"any","val":"${this.state.query_string}"}]}],"order_by":[{"field":"title","direction":"asc"}]}&results_per_page=1000`),
       { method: 'GET' },
     )
       .then(response => response.json())
@@ -130,8 +120,7 @@ class SearchResults extends React.Component {
 
   updateVideoItems() {
     fetch(//eslint-disable-line
-      encodeURI(`http://api.gameframe.online/v1/video?q={"filters":[{"name":"name","op":"like","val":"%${this.state.query_string}%"}],
-                                                         "order_by":[{"field":"name","direction":"asc"}]}&results_per_page=10000`),
+      encodeURI(`http://api.gameframe.online/v1/video?q={"filters":[{"name":"name","op":"like","val":"%${this.state.query_string}%"}],"order_by":[{"field":"name","direction":"asc"}]}&results_per_page=10000`),
       { method: 'GET' },
     )
       .then(response => response.json())
