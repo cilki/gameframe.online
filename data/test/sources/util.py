95c859eafc062d1e54e7f5f45a7fc8ac261fac09
Ekin Soysal <ekinsoysal@hotmail.com>
2018-04-04 12:58:42 -0500

    Added condition testing

--- data/test/sources/util.py
+++ data/test/sources/util.py
@@ -7,44 +7,59 @@ import sys
 sys.path.insert(0, 'data/main/sources')  # TODO
 
 import datetime
+from orm import Game, Developer
 
 from pathlib import Path
 from unittest import main, TestCase
-from util import parse_steam_date, condition_video, condition_article, is_cached
-
+from util import parse_steam_date, condition, condition_heavy, condition_developer
 
 class TestUtil (TestCase):
 
     def test_parse_steam_date(self):
         # Test value
+		
         self.assertEqual(str(datetime.datetime(2017, 3, 8)),
                          str(parse_steam_date("Mar 8, 2017")))
-
         self.assertEqual(str(datetime.datetime(2017, 3, 1)),
                          str(parse_steam_date("Mar 2017")))
+        self.assertEqual(str(datetime.datetime(1968, 2, 28)),
+                         str(parse_steam_date("Feb 28, 1968")))
+        self.assertEqual(str(datetime.datetime(3019, 10, 1)),
+                         str(parse_steam_date("Oct 3019")))
 
     def test_condition(self):
         """
         Test keyword conditioning
         """
 
-        self.assertEqual("Great Title edition", condition_article(
+        self.assertEqual("great title: edition", condition(
             "Great® ®Title™: <sup>edition"))
-        self.assertEqual("No special chars 89", condition_article(
-            "No special chars 89"))
+        self.assertEqual("no special chars 89", condition(
+             "No special chars 89"))
 
-        self.assertEqual("No+special+chars+89", condition_video(
-            "No special chars 89"))
+    def test_condition_heavy(self):
 
-    def test_is_cached(self):
-        """
-        Test cache checking with known values
-        """
+        self.assertEqual("visualizethemovie", condition_heavy(
+             "vi|sua@li!z<e; +the€ mO\v/vie"))
+        self.assertEqual("ringring", condition_heavy(
+             "®Ri NG®;./  <sup><r!@ing?>"))
+        self.assertEqual("_underscores_", condition_heavy(
+             ";-; (: ¯\_(underscores)_/¯"))
+        self.assertEqual("hello", condition_heavy(
+             "<h,e#l!l[o]>'<{?}>"))
+        self.assertEqual("123456789", condition_heavy(
+             "1*2-3+4/5%6^7=8.9"))
 
-        Path('/tmp/574570').touch()
-        self.assertTrue(is_cached("/tmp/", "574570"))
-        self.assertFalse(is_cached("/tmp/", "nonexistent"))
+    def test_condition_developer(self):
 
+        self.assertEqual("ea games" , condition_developer(
+            "EA Games"))
+        self.assertEqual("double:dot arts inc." , condition_developer(
+            "Double:Dot Arts Inc."))
+        self.assertEqual("warlord coorp®" , condition_developer(
+            "Warlord Coorp®"))
+        self.assertEqual("technology™ studios" , condition_developer(
+            "Technology™ Studios"))
 
 if __name__ == '__main__':
     main()
