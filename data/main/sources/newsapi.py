a15165284a69a7fee26f0b7f1124aaa1cf84746f
Tyler Cook <tylercurtiscook@gmail.com>
2018-04-16 22:22:50 -0500

    Refine `newsapi.py`

--- data/main/sources/newsapi.py
+++ data/main/sources/newsapi.py
@@ -1,12 +1,10 @@
 # --------------------------------
-# IGN API scraper                -
+# NewsAPI scraper                -
 # Copyright (C) 2018 GameFrame   -
 # --------------------------------
 
-
 import re
 from datetime import datetime
-from functools import lru_cache
 
 from newsapi import NewsApiClient
 from tqdm import tqdm
@@ -14,18 +12,7 @@ from tqdm import tqdm
 from cache import WS, KeyCache, load_working_set
 from common import TC, load_registry
 from registry import KeyNewsapi, CachedArticle
-
-from .util import condition, condition_heavy, keywordize, url_normalize, xappend
-
-"""
-The API key cache
-"""
-KEYS = KeyCache(KeyNewsapi)
-
-"""
-The NewsAPI client
-"""
-API = NewsApiClient(api_key=KEYS.get())
+from sources.util import condition, condition_heavy, generic_gather, url_normalize, xappend
 
 
 """
@@ -53,56 +40,61 @@ WHITELIST = ['Nintendolife.com', 'Gonintendo.com', 'Playstation.com', 'IGN',
 """
 Keywords that should have very few relevant games
 """
-BLACKLIST_KEYWORDS = ['amazon', 'trump', 'morgage', 'chuckit!', 'walmart']
+BLACKLIST_KEYWORDS = ['amazon', 'trump', 'trump\'s', 'morgage', 'walmart']
 BLACKLIST = re.compile(
     "\W+" + "\W.*$|\W+".join(BLACKLIST_KEYWORDS) + '\W.*$', re.IGNORECASE)
+
+"""
+The API key cache
+"""
+KEYS = KeyCache(KeyNewsapi)
+
+"""
+The NewsAPI client
+"""
+API = NewsApiClient(api_key=KEYS.get())
+
+
+def reload_api():
+    """
+    Reinitialize the API client with a new API key. This method may block if no
+    valid keys are currently available.
+    """
+    global API
+    API = NewsApiClient(KEYS.advance())
 
 
 def rq_articles(game):
     """
     Request articles from NewsAPI
     """
-    global API
-    p = 1
+    page = 1
 
-    while p < MAX_PAGES:
+    while page < MAX_PAGES:
         rq = API.get_everything(language='en', sort_by='relevancy', page_size=100,
-                                page=p, q=game.c_name)
+                                page=page, q=game.c_name)
 
         if rq['status'] == 'error':
-            API = NewsApiClient(KEYS.advance())
-            continue
+            reload_api()
+            return
 
         for article_json in rq['articles']:
 
+            # Unit filtering
             if not validate_article(article_json):
                 continue
 
-            # Filter relevancy
-            name = condition_heavy(game.c_name)
-            if name not in condition_heavy(article_json['title']) and \
-                    name not in condition_heavy(article_json['description']):
+            # Relevancy filtering
+            if not relevant_article(game, article_json):
                 continue
 
             TC['Article.game_id'].add(CachedArticle(
                 game_id=game.game_id, newsapi_data=article_json))
 
-        if rq['totalResults'] <= p * 100:
+        if rq['totalResults'] <= page * 100:
             # That was the last page
-            break
-        p += 1
-
-
-def gather_articles():
-    """
-    Search for articles related to games and download them to the cache
-    """
-    load_working_set()
-    load_registry('Article', 'game_id')
-
-    for game in tqdm(WS.games.values(), '[NEWSAPI ] Gathering Articles'):
-        if not TC['Article.game_id'].exists(game.game_id):
-            rq_articles(game)
+            return
+        page += 1
 
 
 def build_article(article, article_json):
@@ -112,13 +104,30 @@ def build_article(article, article_json):
     if article is None or article_json is None:
         return
 
-    article.outlet = article_json['source']['name']
-    article.introduction = article_json['description']
-    article.author = article_json['author']
-    article.timestamp = datetime.strptime(
-        article_json['publishedAt'], "%Y-%m-%dT%H:%M:%SZ")
-    article.cover = url_normalize(article_json['urlToImage'])
-    article.article_link = url_normalize(article_json['url'])
+    # Outlet name
+    if article.outlet is None:
+        article.outlet = article_json['source']['name']
+
+    # Content
+    if article.introduction is None:
+        article.introduction = article_json['description']
+
+    # Author
+    if article.author is None:
+        article.author = article_json['author']
+
+    # Timestamp
+    if article.timestamp is None:
+        article.timestamp = datetime.strptime(article_json['publishedAt'],
+                                              "%Y-%m-%dT%H:%M:%SZ")
+
+    # Cover
+    if article.cover is None:
+        article.cover = url_normalize(article_json['urlToImage'])
+
+    # External link
+    if article.article_link is None:
+        article.article_link = url_normalize(article_json['url'])
 
 
 def validate_article(article_json):
@@ -138,25 +147,50 @@ def validate_article(article_json):
             return False
 
         # Filter title
-        if len(article_json['title']) < 5:
+        if article_json['title'] is None:
             return False
 
         # Filter Introduction
-        if article_json['description'] is None or len(article_json['description']) < 10:
+        if article_json['description'] is None:
             return False
 
         # Filter Timestamp
-        if len(article_json['publishedAt']) < 5:
+        if article_json['publishedAt'] is None:
             return False
 
         # Filter Image
-        if article_json['urlToImage'] is None or len(article_json['urlToImage']) < 10:
+        if article_json['urlToImage'] is None:
             return False
 
         # Filter Article link
-        if len(article_json['url']) < 10:
+        if article_json['url'] is None:
             return False
 
     except KeyError:
         return False
     return True
+
+
+def relevant_article(game, article_json):
+    """
+    Determine the relevance of an article to a game
+    """
+
+    name = condition_heavy(game.c_name)
+    if name in condition_heavy(article_json['title']) or \
+            name in condition_heavy(article_json['description']):
+        return True
+
+    return False
+
+
+def gather_articles():
+    """
+    Search for articles related to games and download them to the cache
+    """
+    load_working_set()
+    load_registry('Article', 'game_id')
+
+    generic_gather(rq_articles, TC['Article.game_id'], '[GATHER] Downloading Articles',
+                   [game for game in WS.games.values() if not
+                    TC['Article.game_id'].exists(game.game_id)])
