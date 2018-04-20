80a57c4accabc7168f7362c2f89c7de7b52d3dbf
Andrew Rieder <atrieder@utexas.edu>
2018-04-19 18:50:44 -0700

    Refactored Developer.js.

--- src/components/developer/Developer.js
+++ src/components/developer/Developer.js
@@ -35,7 +35,7 @@ link.propTypes = {
   key: PropTypes.oneOf([PropTypes.string, PropTypes.number]).isRequired,
 };
 
-function twitter({ twitterUsername }) {
+function twitterWidget({ twitterUsername }) {
   return (
     <Timeline
       dataSource={{
@@ -51,7 +51,7 @@ function twitter({ twitterUsername }) {
   );
 }
 
-twitter.propTypes = {
+twitterWidget.propTypes = {
   twitterUsername: PropTypes.string.isRequired,
 };
 
@@ -75,6 +75,227 @@ function dateToString(date) {
     `${months[month]} ${day}${sup}, ${year}`
   );
 }
+
+
+function primaryInfoCluster({
+  name,
+  foundation,
+  country,
+}) {
+
+  let iso = require('iso-3166-1'); //eslint-disable-line
+
+  const established = foundation ? dateToString(foundation) : 'Unknown';
+  const countryNumber = `${country}`;
+  const _country = iso.whereNumeric(countryNumber);
+  const countryName = _country ? _country.country : 'Unknown';
+
+  return (
+    <div style={[InstanceDetailsStyles.developerPrimaryInfoCluster]}>
+      <div style={[InstanceDetailsStyles.titleText]}>
+        {name}
+      </div>
+      <div style={[InstanceDetailsStyles.establishDateIndicator]}>
+        {established !== 'Unknown' ? `Established ${established}` : 'Unknown date of establishment.'}
+      </div>
+      <div style={[InstanceDetailsStyles.locationIndicator]}>
+        {countryName !== 'Unknown' ? `Based in ${countryName}` : 'Unkown location.'}
+      </div>
+    </div>
+  );
+}
+
+primaryInfoCluster.propTypes = {
+  name: PropTypes.string,
+  foundation: PropTypes.string,
+  country: PropTypes.string,
+};
+
+function descriptionSection({
+  description,
+}) {
+
+  return (
+    <div>
+      <hr style={[InstanceDetailsStyles.horizontalRule]} />
+      <div style={[InstanceDetailsStyles.synopsisIndicator]}>
+        Description:
+      </div>
+      <div style={[InstanceDetailsStyles.synopsisHTMLContainer]}>
+        {description}
+      </div>
+      <hr style={[InstanceDetailsStyles.horizontalRule]} />
+    </div>
+  );
+}
+
+descriptionSection.propTypes = {
+  description: PropTypes.string,
+};
+
+function websiteButton({
+  website,
+}) {
+
+  if (website) {
+    return (
+      <a href={website} style={[InstanceDetailsStyles.bigButton]} key="website">
+        Developer Website
+      </a>
+    );
+  }
+  return '';
+}
+
+websiteButton.propTypes = {
+  website: PropTypes.string,
+};
+
+function twitterButton({
+  twitter,
+}) {
+
+  if (twitter) {
+    return (
+      <a href={twitter} style={[InstanceDetailsStyles.bigButton]} key="twitter">
+        Developer Twitter
+      </a>
+    );
+  }
+  return '';
+}
+
+twitterButton.propTypes = {
+  twitter: PropTypes.string,
+};
+
+function bigButtonCluster({
+  left,
+  right,
+}) {
+
+  return (
+    <div style={[InstanceDetailsStyles.bigButtonCluster]}>
+      {websiteButton({ website: left })}
+      {twitterButton({ twitter: right })}
+    </div>
+  );
+}
+
+bigButtonCluster.propTypes = {
+  left: PropTypes.object,
+  right: PropTypes.object,
+};
+
+function developerWidgetGroup({
+  twitter,
+  name,
+}) {
+
+  const twitterHandle = typeof twitter === 'string' ? `${twitter}`.replace('https://twitter.com/', '') : '';
+  const twitterDummy = twitterHandle !== '' ? [twitter] : [];
+  const trendName = encodeURI(name);
+  const trendsURL = `https://trends.google.com:443/trends/embed/explore/TIMESERIES?req=%7B%22comparisonItem%22%3A%5B%7B%22keyword%22%3A%22${trendName}%22%2C%22geo%22%3A%22%22%2C%22time%22%3A%22all%22%7D%5D%2C%22category%22%3A0%2C%22property%22%3A%22%22%7D&tz=300&amp;eq=date%3Dall%26q%3D${trendName}`;
+
+  return (
+    <div style={[InstanceDetailsStyles.developerWidgetGroup]}>
+      <div style={[InstanceDetailsStyles.developerTwitterContainer]}>
+        {
+          twitterDummy.map(() => twitterWidget({
+            twitterUsername: twitterHandle,
+          }))
+        }
+      </div>
+      <div style={[InstanceDetailsStyles.googleTrendsContainer]}>
+        <iframe
+          id="trends-widget-1"
+          src={trendsURL}
+          width="100%"
+          frameBorder="0"
+          scrolling="0"
+          style={[InstanceDetailsStyles.googleTrendsIframe]}
+          title="Google Trends"
+        />
+      </div>
+    </div>
+  );
+}
+
+developerWidgetGroup.propTypes = {
+  twitter: PropTypes.string,
+  name: PropTypes.string,
+};
+
+function gameGridCluster({
+  games,
+}) {
+  return (
+
+    <div style={[InstanceDetailsStyles.gameGridCluster('50%')]}>
+      <div style={[InstanceDetailsStyles.gameIndicator]}>
+        Games:
+      </div>
+      <Minigrid>
+        {
+          games.map(game => link({
+            label: game.name,
+            url: `/games/${game.id}`,
+            cover: (game.cover && game.cover.indexOf('http') < 0 ? `https://${game.cover}` : game.cover),
+            key: `game-${game.id}`,
+          }))
+        }
+      </Minigrid>
+    </div>
+  );
+}
+
+gameGridCluster.propTypes = {
+  games: PropTypes.array,
+};
+
+function articleGridCluster({
+  articles,
+}) {
+  return (
+    <div style={[InstanceDetailsStyles.articleGridCluster('50%')]}>
+      <div style={[InstanceDetailsStyles.articleIndicator]}>
+        Articles:
+      </div>
+      <Minigrid>
+        {
+          articles.map(article => link({
+            label: article.title,
+            url: `/articles/${article.id}`,
+            cover: article.cover,
+            key: `article-${article.id}`,
+          }))
+        }
+      </Minigrid>
+    </div>
+  );
+}
+
+articleGridCluster.propTypes = {
+  articles: PropTypes.array,
+};
+
+function modelGridClusters({
+  games,
+  articles,
+}) {
+
+  return (
+    <div style={[InstanceDetailsStyles.externalGridCluster]}>
+      {gameGridCluster({ games: games, })}
+      {articleGridCluster({ articles: articles })}
+    </div>
+  );
+}
+
+modelGridClusters.propTypes = {
+  games: PropTypes.array,
+  articles: PropTypes.array,
+};
 
 /**
  * @description - Returns the main component to render a developer's own
@@ -141,14 +362,6 @@ class Developer extends React.Component {
   render() {
     const logoURL = this.props.logo && this.props.logo.indexOf('http') < 0 ? `https://${this.props.logo}` : this.props.logo;
     const trueLogoURL = this.props.logo == null ? '../../static/images/noImage.png' : logoURL;
-    const established = this.props.foundation ? dateToString(this.props.foundation) : 'Unknown';
-    const countryNumber = `${this.props.country}`;
-    const country = this.iso.whereNumeric(countryNumber);
-    const countryName = country ? country.country : 'Unknown';
-    const twitterHandle = typeof this.props.twitter === 'string' ? `${this.props.twitter}`.replace('https://twitter.com/', '') : '';
-    const twitterDummy = twitterHandle !== '' ? [this.props.twitter] : [];
-    const trendName = encodeURI(this.props.name);
-    const trendsURL = `https://trends.google.com:443/trends/embed/explore/TIMESERIES?req=%7B%22comparisonItem%22%3A%5B%7B%22keyword%22%3A%22${trendName}%22%2C%22geo%22%3A%22%22%2C%22time%22%3A%22all%22%7D%5D%2C%22category%22%3A0%2C%22property%22%3A%22%22%7D&tz=300&amp;eq=date%3Dall%26q%3D${trendName}`;
     return (
       <StyleRoot>
         <Helmet>
@@ -165,96 +378,37 @@ class Developer extends React.Component {
                 />
               </div>
             </div>
-            <div style={[InstanceDetailsStyles.developerPrimaryInfoCluster]}>
-              <div style={[InstanceDetailsStyles.titleText]}>
-                {this.props.name}
-              </div>
-              <div style={[InstanceDetailsStyles.establishDateIndicator]}>
-                {established !== 'Unknown' ? `Established ${established}` : 'Unknown date of establishment.'}
-              </div>
-              <div style={[InstanceDetailsStyles.locationIndicator]}>
-                {countryName !== 'Unknown' ? `Based in ${countryName}` : 'Unkown location.'}
-              </div>
-            </div>
-          </div>
-          <div>
-            <hr style={[InstanceDetailsStyles.horizontalRule]} />
-            <div style={[InstanceDetailsStyles.synopsisIndicator]}>
-                Description:
-            </div>
-            <div style={[InstanceDetailsStyles.synopsisHTMLContainer]}>
-              {this.props.description}
-            </div>
-            <hr style={[InstanceDetailsStyles.horizontalRule]} />
-          </div>
-          <div style={[InstanceDetailsStyles.bigButtonCluster]}>
             {
-              this.props.website ? (
-                <a href={this.props.website} style={[InstanceDetailsStyles.bigButton]} key="website">
-                  Developer Website
-                </a>
-              ) : ''
-            }
-            {
-              this.props.twitter ? (
-                <a href={this.props.twitter} style={[InstanceDetailsStyles.bigButton]} key="twitter">
-                  Developer Twitter
-                </a>
-              ) : ''
+              primaryInfoCluster({
+                name: this.props.name,
+                foundation: this.props.foundation,
+                country: this.props.country,
+              })  
             }
           </div>
-          <div style={[InstanceDetailsStyles.developerWidgetGroup]}>
-            <div style={[InstanceDetailsStyles.developerTwitterContainer]}>
-              {
-                twitterDummy.map(() => twitter({
-                  twitterUsername: twitterHandle,
-                }))
-              }
-            </div>
-            <div style={[InstanceDetailsStyles.googleTrendsContainer]}>
-              <iframe
-                id="trends-widget-1"
-                src={trendsURL}
-                width="100%"
-                frameBorder="0"
-                scrolling="0"
-                style={[InstanceDetailsStyles.googleTrendsIframe]}
-                title="Google Trends"
-              />
-            </div>
-          </div>
-          <div style={[InstanceDetailsStyles.externalGridCluster]}>
-            <div style={[InstanceDetailsStyles.gameGridCluster('50%')]}>
-              <div style={[InstanceDetailsStyles.gameIndicator]}>
-                Games:
-              </div>
-              <Minigrid>
-                {
-                  this.props.games.map(game => link({
-                    label: game.name,
-                    url: `/games/${game.id}`,
-                    cover: (game.cover && game.cover.indexOf('http') < 0 ? `https://${game.cover}` : game.cover),
-                    key: `game-${game.id}`,
-                  }))
-                }
-              </Minigrid>
-            </div>
-            <div style={[InstanceDetailsStyles.articleGridCluster('50%')]}>
-              <div style={[InstanceDetailsStyles.articleIndicator]}>
-                Articles:
-              </div>
-              <Minigrid>
-                {
-                  this.props.articles.map(article => link({
-                    label: article.title,
-                    url: `/articles/${article.id}`,
-                    cover: article.cover,
-                    key: `article-${article.id}`,
-                  }))
-                }
-              </Minigrid>
-            </div>
-          </div>
+          {
+            descriptionSection({
+              description: this.props.description,
+            })
+          }
+          {
+            bigButtonCluster({
+              left: this.props.website,
+              right: this.props.twitter,
+            })
+          }
+          {
+            developerWidgetGroup({
+              twitter: this.props.twitter,
+              name: this.props.name,
+            })
+          }
+          {
+            modelGridClusters({
+              games: this.props.games,
+              articles: this.props.articles,
+            })
+          }
         </InstanceDetails>
       </StyleRoot>
     );
